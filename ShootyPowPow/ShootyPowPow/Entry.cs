b1012d80abc6baefcd64906bea57ce7087bbfa83
Josh Dadak <Bigtalljosh@users.noreply.github.com>
2015-11-22 20:03:39 +0000

    Initial Commit
    This is TeamWorkingAllTogether with the updated project to work past the
    VS2015 RC

---
+++ ShootyPowPow/ShootyPowPow/Entry.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShootyPowPow
+{
+    class Entry
+    {
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (Game1 game = new Game1())
+                {
+                    game.Run();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed To Start Program" + "\n \n" + ex.Message + "\n \n" + ex.InnerException);
+            }
+
+        }
+    }
+}
